5177aa93a028b89fac1b48bf18a66c67e8b0747c
Jakub Kozłowski <kubukoz@gmail.com>
2021-02-21 21:36:50 +0100

    Remove deps

--- build.sbt
+++ build.sbt
@@ -5,7 +5,5 @@ lazy val root = project
   .settings(
     name := "scala3-simple",
     version := "0.1.0",
-    scalaVersion := scala3Version,
-    libraryDependencies +=
-      "io.circe" %% "circe-core" % "0.14.0-M3"
+    scalaVersion := scala3Version
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,14 @@
-import io.circe._
+type Id[A] = A
 
-enum Expr[F[_]] derives Codec.AsObject:
+trait Codec[A]
+object Codec:
+  def derived[A]: Codec[A] = ???
+
+
+enum Expr[F[_]] derives Codec:
   case Term(name: F[String])
   case Apply(on: Expr[F], param: Expr[F])
 
 //this works:
 // object Expr:
-//   given Codec.AsObject[Expr[cats.Id]] = Codec.AsObject.derived
+//   given Codec[Expr[Id]] = Codec.derived
